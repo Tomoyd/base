b6dd5b878f30298b2b2bd285bcfeaeb49c5b28e8
tomoyd <ydh9419@163.com>
2022-03-30 14:20:58 +0800

    codebyHand:big-add

---
+++ codeByHand/sum.js
@@ -0,0 +1,40 @@
+function add(a = '', b) {
+	if (!a || !b) {
+		return NaN;
+	}
+
+	let result = [];
+
+	let jin = 0;
+
+	let aIndex = a.length - 1;
+	let bIndex = b.length - 1;
+	let bigIndex = aIndex > bIndex ? aIndex : bIndex;
+
+	while (bigIndex >= 0) {
+		const r = Number(b[bIndex--] || 0) + Number(a[aIndex--] || 0) + jin;
+
+		if (bigIndex === 0) {
+			result.unshift(r);
+			return result.join('');
+		}
+
+		if (r >= 10) {
+			jin = 1;
+			result.unshift(r - 10);
+		} else {
+			jin = 0;
+			result.unshift(r);
+		}
+
+		bigIndex--;
+	}
+}
+
+console.log(
+	'111+999',
+	add(
+		'1111111111111111111111111111111111111111111111111111111111',
+		'9999999999999999999999999999999999999999999999999999999999',
+	),
+);
